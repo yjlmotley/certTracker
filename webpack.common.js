a7098b73790c2d6b98bca745a18ee0fd2d008b0c
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2025-02-12 14:42:39 +0000

    updated icon for website

--- webpack.common.js
+++ webpack.common.js
@@ -14,40 +14,40 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          // exclude: /node_modules/,
-          exclude: /node_modules\/(?!react-dnd)/, // Allows react-dnd
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(css|scss)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        {
-          test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' }
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
+      {
+        test: /\.(js|jsx)$/,
+        // exclude: /node_modules/,
+        exclude: /node_modules\/(?!react-dnd)/, // Allows react-dnd
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/, use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }]
+      }, //css only files
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
+          loader: 'file-loader',
+          options: { name: '[name].[ext]' }
+        }
+      }, //for images
+      { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
     ]
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
     fullySpecified: false,
     fallback: {
-        'react/jsx-runtime': 'react/jsx-runtime.js',
-        'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
+      favicon: 'cust_cert.ico',
+      template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
   ]
